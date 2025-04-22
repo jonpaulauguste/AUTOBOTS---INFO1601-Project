eb17b358820d4cce56517460da578ccbe0cfe7e8
jonpaulauguste <144620333+jonpaulauguste@users.noreply.github.com>
2025-04-21 20:37:16 -0400

    test
    test

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";  // Import Firebase Auth
+import { getFirestore } from "firebase/firestore";  // Import Firestore
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAKp9EPhSPOc55d4GcQPmpNwfE5RWlZpkg",
+  apiKey: "AIzaSyAKp9EPhSPOc55d4GcQPmpNwfE5RWlZpkg", 
   authDomain: "autobots---info1601.firebaseapp.com",
   projectId: "autobots---info1601",
-  storageBucket: "autobots---info1601.appspot.com",
+  storageBucket: "autobots---info1601.firebasestorage.app",
   messagingSenderId: "344675316997",
   appId: "1:344675316997:web:4f3c7468a392289104736c",
   measurementId: "G-63BCPY2NSV"
@@ -15,7 +17,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and Firestore
+const auth = getAuth(app);  // For Authentication
+const db = getFirestore(app);  // For Firestore
 
 export { auth, db };
